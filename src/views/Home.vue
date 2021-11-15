b7ed8c69476e9196eb00dfddba31d843c99bf745
fikrahadi <13518036@std.stei.itb.ac.id>
2021-11-15 16:47:31 +0700

    second init

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <div class="content">
+      <p class="title">INI HOME</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  methods: {
+  }
+}
+</script>
+
+<style >
+.container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+}
+.content {
+  width: 400px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+}
+.title {
+  font-size: 32px;
+}
+</style>
