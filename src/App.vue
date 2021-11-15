f0d5d32fffb0ace71c0a1a1d8d0e56806261cc76
fikrahadi <13518036@std.stei.itb.ac.id>
2021-11-15 17:37:16 +0700

    init navbar

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,12 @@ export default {
 </script>
 
 <style>
+@font-face {
+  font-family: "Rubik";
+  src: local("Rubik"),
+   url(./fonts/Rubik-Regular.ttf) format("truetype")
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: "Rubik";
 }
 </style>
---
+++ src/components/Navbar/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div :class="$style.navbar__container">
+    <div :class="$style.navbar__content">
+      <img :class="$style.logo" src="" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="sass" module>
+
+</style>
