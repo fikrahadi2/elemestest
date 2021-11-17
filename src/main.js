0908890c3a668f7837d75ce2d82d649600f21d83
fikrahadi <13518036@std.stei.itb.ac.id>
2021-11-17 13:04:21 +0700

    fix bug

--- src/main.js
+++ src/main.js
@@ -2,11 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueStarRating from 'vue-star-rating'
-//import * as VueSvgLoader from 'vue-svg-loader'
-import "./icons"
 
 const app = createApp(App);
 app.component('star-rating', VueStarRating)
-//app.use(VueSvgLoader)
 app.use(router)
 app.mount('#app')
